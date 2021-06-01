ede9b2a36ac00d364c631748fc7e7a82286c4655
Kevin <ludwigvanharrigan@gmail.com>
2021-06-01 17:05:25 -0400

    Improve crop and frame size reporting, and add line image generator

--- WrenchModeSlopeFinder.py
+++ WrenchModeSlopeFinder.py
@@ -3,6 +3,8 @@ import imutils
 import cv2
 import numpy as np
 from scipy import stats
+from math import atan, atan2, degrees
+
 
 #To do:
 # - Organize this mess. Could we use an object oriented structure?
@@ -20,13 +22,12 @@ def findSlope (point1, point2):
 	rise = point2[1]-point1[1] #Point[1] is the distance DOWN from the top left corner, aka -Y, but I'll call it Y for now.
 	#Downward slopes will be positive.
 	
-	
 	rise = float(rise) #Because I don't know how smart Python is
 	run = float(run) 
 	if(not run == 0):
 		slope = rise/run
 	else:
-		slope = "Vertical"
+		slope = float('inf') #Python's Infinity!
 	
 	#---DEBUG STUFF---
 	#print("Rise: " + str(rise))
@@ -36,9 +37,9 @@ def findSlope (point1, point2):
 	return slope 
 	
 	
-def contourToSlopes (contour, pointSkip=4):
-	
 
+	
+def contourToSlopes (contour, pointSkip=4):
 	
 	subDivisions = int(np.ceil(len(contour)/pointSkip)) #If we count by pointSkip, we count this many items. The ceil is to make it a nice number that we can use it in a for loop. Note that since we're always rounding up, and we're multiplying by the same number we just divided by (later), we won't do a indexOutOfBounds.
 	slopes = [] ; # All the slopes in one place
@@ -60,29 +61,39 @@ def generateLineContour (point1, point2): #Makes a contour that can be added to 
 	return np.array([[point1],[point2]]) #Note the extra set of brackets around each point!!
 	
 
-
-
-
+def generateLineimage():
+	point1 = (0,0)
+	point2 = (500,500)
+	
+	return cv2.line(np.zeros((v_res, h_res,1), dtype = "uint8"), point1, point2, color=255, thickness=300)
+	
 
 
 cam = cv2.VideoCapture(0)
-v_res = 480 #Set resolution
-h_res = 640
-v_crop = 720
+cam_h_res = 1280  
+cam_v_res = 720 #Set resolution. This used to be called v_res.
 h_crop = 1000
+v_crop = 720
+h_res = h_crop if (h_crop < cam_h_res) else cam_h_res #The actual final resolution
+v_res = v_crop if (v_crop < cam_v_res) else cam_v_res
+
+
+
+
 
 #Where's the middle?
-v_half = int(v_res/2)
-h_half = int(h_res/2)
-v_half_crop = int(v_crop/2)
-h_half_crop = int(h_crop/2)
-center_x = h_half #This will change only if we crop
-center_y = v_half
+cam_h_half = int(cam_h_res/2)
+cam_v_half = int(cam_v_res/2)
+h_half_crop = min(int(h_crop/2), cam_h_half)
+v_half_crop = min(int(v_crop/2), cam_v_half) #The min check is because we'll never have a crop bigger than the source frame
+center_x = h_half_crop if (h_crop < cam_h_res) else cam_h_half #the actual final center. May be redundant.
+center_y = v_half_crop if (v_crop < cam_v_res) else cam_v_half
 
 
 
-cam.set(3, h_res)  # Set horizontal resolution
-cam.set(4, v_res)  # Set vertical resolution
+
+cam.set(3, cam_h_res)  # Set horizontal resolution
+cam.set(4, cam_v_res)  # Set vertical resolution
 sleep(2.0)
 
 
@@ -90,29 +101,32 @@ sleep(2.0)
 while(True):
 	ret, frame = cam.read() #Not sure what the first returned thingy is
 	
-	if (v_crop < v_res) or (h_crop < h_res): #Only krop if we need to!
-		frame = frame[v_half-v_half_crop : v_half+v_half_crop, h_half-h_half_crop : h_half+h_half_crop] #Krop!
-		center_y = v_half_crop
-		center_x = h_half_crop
+	if (v_crop < cam_v_res) or (h_crop < cam_h_res): #Only krop if we need to!
+		frame = frame[cam_v_half-v_half_crop : cam_v_half+v_half_crop, cam_h_half-h_half_crop : cam_h_half+h_half_crop] #Krop!
 		
 	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 	blurred = cv2.GaussianBlur(gray, (5, 5), 0)
 	thresh = cv2.threshold(blurred, 80,255,cv2.THRESH_BINARY)[1] #Simple binary threshold; anything over 60 gets turned to 255, anything under 60 gets turned to 0
 	thresh = cv2.bitwise_not(thresh) #invert the image (black to white, since black is 0 and findContours looks for anything BUT 0)
-	contours = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE) #Finds contours in the image. The 2nd and 3rd arguments are  the retrieval mode and contour simplification mode respectively. I'm not sure why it requires a .copy() of the image... maybe it tries to change it? 
+	
+	#Finds contours in the image. The 2nd and 3rd arguments are  the retrieval mode and contour simplification mode respectively. 
+	contours = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE) 
+	#I'm not sure why it requires a .copy() of the image... maybe it tries to change it? 
 	contours = imutils.grab_contours(contours) #parse the output of findContours with Adrian (from PyImageSearch)'s fancy imutils library
 
 # Get a list of the areas of all the contours (in preparation of picking the biggest one)
 	
 	largest_contour_index = 0 #declare this stuff globally!
 	largest_contour = 0
-	for i, contour in enumerate(contours): #enumerate(things) returns (i, thing) where i is the index of thing in things. This lets us remember exactly WHERE the biggest contour is, not just WHAT it is.
+	for i, contour in enumerate(contours): #enumerate(things) returns (i, thing) where i is the index of thing in things. 
+	#This lets us remember exactly WHERE the biggest contour is, not just WHAT it is.
 		contour_area = cv2.contourArea(contour) #How big is the contour?
 		if  contour_area > largest_contour: #If we have a new champion...
 			largest_contour = contour_area # give it the belt
 			largest_contour_index = i # ...and get its address (for the paparazzi)
 	# Then keep looping until the absolute best champion has been found
-	# Note that now, largest_contour will get us the largest contour's area, but we can also get it with cv2.contourArea(contours[largest_contour_index])
+	# Note that now, largest_contour will still get us the largest contour's area
+	# We can also get it with cv2.contourArea(contours[largest_contour_index])
 
 
 
@@ -124,7 +138,7 @@ while(True):
 		#The point holder is the second [0], and 'point holder' is just my name for it.
 		#I bet it can hold something else...
 		contour = contours[largest_contour_index] 
-		slopeys = contourToSlopes(contour) 
+		slopeys = contourToSlopes(contour, pointSkip=10) 
 		mode, count = stats.mode(slopeys)
 		slopesMode = float(mode[0]) #Because I don't know how smart stats.mode() is
 		#print("Mode Slope: " + str(slopesMode))
@@ -136,15 +150,15 @@ while(True):
 		#drawn_contour = [box_points] #Since drawContours() expects an array of contours (arrays of points) and boxPoints() returns an array of points, we must put it in another layer of list. Note that if we want to add anything else to be drawn, we can simply append it to drawn_contour.
 		drawn_contour = contours
 		
-		
+		'''
 		x2 = center_x + 100
 		y2 = center_y + int(np.round(100*slopesMode))
 		slopeLine = generateLineContour([center_x, center_y], [x2, y2])
 	
-		print(slopeLine)
+		#print(slopeLine)
 		drawn_contour.append(slopeLine)
-		
-		
+		'''
+		print(degrees(atan(slopesMode))) # Degrees of the allen wrench. Note that positive angles are pointing down.
 		
 		
 		cv2.drawContours(image=frame, contours=drawn_contour, contourIdx=-1, color=(0,255,0), thickness=1)
